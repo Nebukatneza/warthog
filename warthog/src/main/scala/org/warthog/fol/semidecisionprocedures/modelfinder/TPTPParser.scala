820af4ab9ce5312fbc97a7f13b665189beb2aa03
Nebukatneza <constibaer@web.de>
2012-07-13 17:26:52 +0200

    first prototyp of Modelchecker implemented

---
+++ warthog/src/main/scala/org/warthog/fol/semidecisionprocedures/modelfinder/TPTPParser.scala
@@ -0,0 +1,13 @@
+package org.warthog.fol.semidecisionprocedures.modelfinder
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nebu
+ * Date: 10.07.12
+ * Time: 14:07
+ * To change this template use File | Settings | File Templates.
+ */
+
+object TPTPParser {
+
+}
